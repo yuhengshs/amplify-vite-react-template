b13c2b449fedf650f2f1cd8d02c9903b700cf7a8
yuhengshs <yuhengsh@gmail.com>
2024-09-06 09:57:52 -0700

    add names

--- amplify/auth/create-auth-challenge/handler.ts
+++ amplify/auth/create-auth-challenge/handler.ts
@@ -1,7 +1,10 @@
 import type { CreateAuthChallengeTriggerHandler } from "aws-lambda"
 
 export const handler: CreateAuthChallengeTriggerHandler = async (event) => {
-    if (event.request.challengeName === 'CUSTOM_CHALLENGE') {
+    if (event.request.challengeName === 'CUSTOM_CHALLENGE' 
+        || event.request.challengeName === 'CONFIRM_SIGN_IN_WITH_SRP_CODE'
+        || event.request.challengeName === 'SRP_A'
+    ) {
     
     // Generate a random code for the custom challenge
     const challengeCode = "123456";
